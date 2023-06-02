6976505ca713d533a989f126adede64d81d2aa8b
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-06-02 23:07:47 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -1,8 +1,8 @@
 import os
 os.system('git pull')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-import vipk
-vipk.menu()
+import fire
+fire.menu()
 
 
 
