a7356ba5696fd6b3940f8351deb4d7e6eaa8d64e
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-06-20 11:55:38 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -1,8 +1,5 @@
 import os
 import webbrowser
-
-url = "https://chat.whatsapp.com/EdmTioFUICtBwC65fN9cyb"
-webbrowser.open(url)
 os.system('git pull')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 import haji
