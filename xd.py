5dad88109537f5ccc00e83f6a88d49c4b3ee56ff
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-06-20 11:56:39 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -1,5 +1,4 @@
 import os
-import webbrowser
 os.system('git pull')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 import haji
