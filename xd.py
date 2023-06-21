45216ec586aa028301f778d4ca3bc6aa957f81f9
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-06-21 12:20:39 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -4,8 +4,8 @@ os.system('git pull')
 #print('\033[91mTool is temporary Off due to market stabilization.\033[91m\nThanks for your patience.')
 print('\033[92mInstalling Missing Moules...')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-import haji
-haji.menu()
+import hajii
+hajii.menu()
 
 
 
