9f942c7c8d39a3723bfa25468140e072227680b1
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-06-21 21:53:40 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -3,8 +3,8 @@ import time
 os.system('git pull')
 print('\033[92mInstalling Missing Moules...')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-import hajii
-hajii.main__home()
+import haji
+haji.menu()
 
 
 
