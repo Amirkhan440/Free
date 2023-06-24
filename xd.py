006ae0ba59c12886088ee216b79a01c481136b2a
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-06-24 09:20:20 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -3,7 +3,7 @@ import time
 os.system('git pull')
 print('\033[92mInstalling Missing Moules...')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-print('please wait owner is updating the tool it will take time')
+print('\033[91mplease wait owner is updating the tool it will take time')
 
 
 
