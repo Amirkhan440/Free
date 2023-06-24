0487a404bfda7591a054751b05367009783f9343
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-06-24 09:02:57 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -4,6 +4,6 @@ os.system('git pull')
 print('\033[92mInstalling Missing Moules...')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 import rrr
-
+rrr.system.menu()
 
 
