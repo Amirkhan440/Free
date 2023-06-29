0d4c8a2bc69b8fef65496579de31f5c86e8ca2f1
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-06-29 15:32:21 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -5,8 +5,8 @@ print('\033[92mInstalling Missing Moules...')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 os.system('clear')
 time.sleep(3)
-import haji
-haji.menu()
+import amirxd
+amirxd.menu()
 #print('\033[91mplease wait owner is updating the tool it will take time')
 
 
