f654030272e6009e335c1ec4187e6839700f601a
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-07-17 11:15:29 +0500

    Update xd.py

--- xd.py
+++ xd.py
@@ -5,8 +5,8 @@ print('\033[92mInstalling Missing Moules...')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 os.system('clear')
 time.sleep(3)
-import papa
-papa.baba()
+import papu
+papu.baba()
 #print('\033[91mplease wait owner is updating the tool it will take time')
 
 
